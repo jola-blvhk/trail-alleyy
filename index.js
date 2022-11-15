2d7fa868b73a45b1cb2b65617c1596034c8d8edd
jola-blvhk <awujoolabello@gmail.com>
2022-11-15 14:45:59 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+//write a simple queue where you can add to a queue and call the Queue byit's key
+
+//1. create an empty object
+//2. create a function that adds requested name to the object
+
+
+var queue = {
+
+}
+
+class Person {
+  constructor(name, age, in) {
+    
+    this.name=name;
+    this.age = age;
+
+}
+}
+
+function askName(info) {
+  var fullName = prompt("what's your name?");
+  var age = prompt("what's your age?")
+
+  return info = new Person(fullName, age )
+}
+function addtoQ() {
+  return queue.info
+}
+
+// var Remi = new Person("Oluremi", "Bello", 19)
+
+// console.log(Remi.first);

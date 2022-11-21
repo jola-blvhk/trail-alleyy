acc0d5e50c0fca680c75a6ea0d2c492b11790faf
jola-blvhk <awujoolabello@gmail.com>
2022-11-21 10:59:33 +0100

    all changes

--- index.js
+++ index.js
@@ -1,32 +1,28 @@
-//write a simple queue where you can add to a queue and call the Queue byit's key
-
-//1. create an empty object
-//2. create a function that adds requested name to the object
 
 
-var queue = {
+class Queue {
+  constructor(element) {
+    this.element = {};
+    this.head = 0;
+    this.tail = 0;
+  }
 
+  //add to the queue
+  enqueue(element) {
+    this.elements.[this.tail] = element;
+    this.tail++;
+  }
+
+  //remove from the queue 
+  dequeue(element) {
+    this.elements[this.head] = element;
+    delete element;
+    this head++;
+
+  }
 }
 
-class Person {
-  constructor(name, age, in) {
-    
-    this.name=name;
-    this.age = age;
+var Remi = 3
 
-}
-}
 
-function askName(info) {
-  var fullName = prompt("what's your name?");
-  var age = prompt("what's your age?")
-
-  return info = new Person(fullName, age )
-}
-function addtoQ() {
-  return queue.info
-}
-
-// var Remi = new Person("Oluremi", "Bello", 19)
-
-// console.log(Remi.first);
+console.log(Remi)
